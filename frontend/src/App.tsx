db38500a3031a860f9cc7b9f72e02d01c8389ea1
João Guilherme <souza.joaoguilherme2002@gmail.com>
2021-05-09 19:21:54 -0300

    home navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Routes from "Routes";
+
 function App() {
   return (
     <Routes />
--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,7 +1,10 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 
+import {BrowserRouter} from "react-router-dom";
+import {Route} from "react-router-dom";
+import {Switch} from "react-router-dom";
+ 
 function Routes() {
     return (
         <BrowserRouter>
